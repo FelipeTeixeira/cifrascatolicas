5d184511dd6ccf9bd686f2f1e6bebe0a836d9368
Felipe Teixeira <felipesantosteixeira@outlook.com>
2023-06-16 16:22:30 -0300

    tela da musica

--- src/pages/musica.tsx
+++ src/pages/musica.tsx
@@ -18,9 +18,10 @@ import { useEffect, useState } from 'react';
 import { Cipher } from '@components/cipher/cipher';
 import { Chords } from '@components/chords/chords';
 import { RadioGroup } from '@components/radio-group/radio-group';
+import { Footer } from '@components/footer/footer';
 
 export default function Musica() {
-    const chords = ['Am', 'Bb2 ', 'C ', 'Dm ', 'F9 ', 'Bb2 ', 'C ', 'Am', 'F9'];
+    const chords = ['Am', 'Bb2 ', 'C ', 'Dm ', 'F9 ', 'Bb2 ', 'C '];
     const [product, setProduct] = useState<any>();
 
     useEffect(() => {
@@ -39,40 +40,62 @@ export default function Musica() {
             <Header />
             <SubHeader />
 
-            <main>
-                <section className={styles.section}>
-                    <Video />
-                </section>
+            <main className={styles.main}>
+                <div className={styles.tools}>
+                    <section className={styles.section}>
+                        <Video />
+                    </section>
 
-                <section className={styles.actions}>
-                    <RadioGroup
-                        selected={'Violão'}
-                        options={['Violão', 'Guitarra', 'Cavaco', 'Teclado']}
-                    />
+                    <section className={`${styles.actions} ${styles.border}`}>
+                        <RadioGroup
+                            selected={'Violão'}
+                            options={['Violão', 'Guitarra', 'Cavaco', 'Teclado']}
+                        />
 
-                    <ToggleButton
-                        onClick={(event: string) => console.log(event)}
-                        options={['Original', 'Simplificada']}
-                    />
-                </section>
+                        <ToggleButton
+                            onClick={(event: string) => console.log(event)}
+                            options={['Original', 'Simplificada']}
+                        />
+                    </section>
 
-                <Chords options={chords} />
+                    <section>
+                        <Chords options={chords} className={styles.chords} />
+                    </section>
 
-                <section className={`${styles.actions} ${styles.actions__counterButton}`}>
-                    <CounterButton name='Tom' />
-                    <CounterButton name='Letra' />
-                </section>
+                    <section className={styles.counterButtonSection}>
+                        <section className={styles.border}>
+                            <span className={styles.label}>Ajuste o tom</span>
+                            <CounterButton name='Tom' />
+                        </section>
 
-                <section className={`${styles.section} ${styles.cipherSection}`}>
-                    {product &&
-                        <Cipher cipher={product.cifra} />
-                    }
-                </section>
+                        <section className={styles.border}>
+                            <span className={styles.label}>Ajuste o tamanho da letra</span>
+                            <CounterButton name='Letra' />
+                        </section>
+                    </section>
 
-                <Toolbar />
+                    <section className={styles.border}>
+                        <span className={styles.label}>Ative a auto rolagem</span>
+                        <Toolbar />
+                    </section>
 
-                <AdvertisingSection />
-            </main >
+                </div>
+
+                <div className={styles.content}>
+                    <section className={`${styles.section} ${styles.cipherSection}`}>
+                        <h1 className={styles.title}>
+                            Água viva
+                            <strong>Adriana Gil</strong>
+                        </h1>
+
+                        {product &&
+                            <Cipher cipher={product.cifra} />
+                        }
+                    </section>
+                    <AdvertisingSection hasPadding={false} />
+                </div>
+            </main>
+            <Footer />
         </>
     )
 }
