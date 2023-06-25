987d6c78858a7102c176a3d7bc1be8ff228b69fb
Felipe Teixeira <felipesantosteixeira@outlook.com>
2023-06-25 15:54:40 -0300

    melhoria quando a url vem diferente

--- src/core/utils/get-video-id.util.ts
+++ src/core/utils/get-video-id.util.ts
@@ -1,6 +1,14 @@
 export function getVideoId(video: string): string {
-    const videoId = video.split('v=')[1];
-    const ampersandPosition = videoId.indexOf('&');
-    const versaoString = ampersandPosition !== -1 ? videoId.substring(0, ampersandPosition) : videoId;
-    return versaoString;
+    if (video.includes('youtu.be')) {
+        return video.split('youtu.be').at(-1) as string;
+    } else {
+        const videoId = video.split('v=')[1];
+        const ampersandPosition = videoId.indexOf('&');
+        const versaoString =
+            ampersandPosition !== -1
+                ? videoId.substring(0, ampersandPosition)
+                : videoId;
+
+        return versaoString;
+    }
 }
