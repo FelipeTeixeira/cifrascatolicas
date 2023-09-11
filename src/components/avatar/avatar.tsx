fbe32e57bb6589f658000a6ff9ff6d7f3f42fc54
Felipe Teixeira <felipesantosteixeira@outlook.com>
2023-09-11 16:09:44 -0300

    limpar imagem quando ja carregado

--- src/components/avatar/avatar.tsx
+++ src/components/avatar/avatar.tsx
@@ -1,6 +1,7 @@
 import Image, { StaticImageData } from 'next/image'
 import styles from './avatar.module.scss'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
 // TODO - deixar path da imagem fixa na string
 // https://cifrascatolicas.com.br/imagens/albums/200/${slug}.png
@@ -10,6 +11,19 @@ export function Avatar(props: {
     className: string;
 }) {
     const [hideImage, setHideImage] = useState(false);
+    const router = useRouter();
+
+    useEffect(() => {
+        const handleRouteChange = () => {
+            setHideImage(false);
+        };
+
+        router.events.on('routeChangeComplete', handleRouteChange);
+
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange);
+        };
+    }, [router.events]);
 
     return (
         <>
