ac1f4c70c7cf953d5a718ed1c235071405801f7c
Felipe Teixeira <felipesantosteixeira@outlook.com>
2023-09-12 09:08:32 -0300

    teste menu

--- src/components/menu-desktop/menu-desktop.tsx
+++ src/components/menu-desktop/menu-desktop.tsx
@@ -8,7 +8,7 @@ export function MenuDesktop(props: {
 }) {
     const router = useRouter();
     const menus = [
-        { name: 'Repertório', url: '/repertorio' },
+        { name: 'Repertório -----', url: '/repertorio' },
         { name: 'Artistas', url: '/artistas' },
         { name: 'Mais acessadas', url: '/musicas-mais-acessadas' }
     ];
