16d7fe8ee43e652314c5bdb18f62209f79b95d06
Felipe Teixeira <felipesantosteixeira@outlook.com>
2023-09-16 06:11:11 -0300

    favicons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,13 @@ export default function Document() {
                 <link rel="preconnect" href="https://fonts.gstatic.com" cross-origin="true" />
                 <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Nunito+Sans:wght@400;600;700&display=swap" rel="stylesheet" />
                 <meta name="theme-color" content="#b69548" />
+
+                <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+                <link rel="manifest" href="/favicons/site.webmanifest" />
+                <meta name="msapplication-TileColor" content="#b69548" />
+
             </Head>
             <body>
                 <Main />
