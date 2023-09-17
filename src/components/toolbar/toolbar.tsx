7fbe18f22662eccce38b6f837016d81b02644a5a
Felipe Teixeira <felipesantosteixeira@outlook.com>
2023-09-17 15:52:11 -0300

    auto rolagem

--- src/components/toolbar/toolbar.tsx
+++ src/components/toolbar/toolbar.tsx
@@ -1,22 +1,62 @@
 import styles from './toolbar.module.scss'
 import { PlayIcon } from '@components/icons/play-icon'
+import { useEffect, useState } from 'react';
 
 export function Toolbar() {
+    const [isScrolling, setIsScrolling] = useState(false);
+    const [speed, setSpeed] = useState(100);
+    const speedOptions = [100, 50, 10, 5];
+    const [speedIndex, setSpeedIndex] = useState(0);
+
+    useEffect(() => {
+        let scrollInterval: NodeJS.Timer;
+
+        if (isScrolling) {
+            scrollInterval = setInterval(() => {
+                window.scrollBy(0, 1);
+
+                if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+                    clearInterval(scrollInterval);
+                    setIsScrolling(false);
+                }
+            }, speed);
+        }
+
+        return () => {
+            clearInterval(scrollInterval);
+        };
+    }, [isScrolling, speed]);
+
+    const toggleScroll = () => {
+        setIsScrolling(!isScrolling);
+    };
+
+    const changeSpeed = () => {
+        const nextSpeedIndex = (speedIndex + 1) % speedOptions.length;
+        setSpeedIndex(nextSpeedIndex);
+        setSpeed(speedOptions[nextSpeedIndex] as any);
+    };
+
     return (
         <div className={styles.toolbar}>
             <p>
                 Auto rolagem
             </p>
 
-            <button className={styles.button} onClick={() => alert('teste')} type="button">
+            <button className={styles.button} onClick={toggleScroll} type="button">
                 <PlayIcon />
             </button>
 
-            <button className={`${styles.button} ${styles.secondary}`} onClick={() => alert('teste')} color='white' type="button">
-                1x
+            <button
+                onClick={changeSpeed}
+                className={`${styles.button} ${styles.secondary}`}
+                color='white'
+                type="button">
+
+                {speedOptions.findIndex(option => option === speed) + 1}x
             </button>
 
-            {/* BOTAO DE SUBIR A TELA */}
+            {/* TODO - BOTAO DE SUBIR A TELA */}
         </div>
     )
 }
