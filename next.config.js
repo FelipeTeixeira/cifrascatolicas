a4d397498f7d7689e477360ed18acaedaeca9705
Felipe Teixeira <felipesantosteixeira@outlook.com>
2023-10-08 09:34:32 -0300

    add new path

--- next.config.js
+++ next.config.js
@@ -3,11 +3,16 @@
     - quando mexer na paginação, exibir um loading de alguma forma.
     - criar modal frella do Txai, add no final da pagina
     - cadastrar repertorio no storage
-
-    - fazer acoes da tela da musica.
     - fazer modal de busca funcionar com google
     - sujerir pra Clara botoes do lado
 
+
+
+    - fazer acoes da tela da musica.
+        precisa do back-end
+            - TOM
+            - acordes
+            - original e simplificada
 
     BOTAO VOLTAR, testar depois quando tiver mais paginas
         const previousUrl = context.req.headers.referer ? new URL(context.req.headers.referer).pathname : `/${context.resolvedUrl.split('/')[1]}`;
@@ -23,6 +28,12 @@ const nextConfig = {
             {
                 protocol: 'https',
                 hostname: 'cifrascatolicas.com.br',
+                port: '',
+                pathname: '/imagens/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'static.cifrascatolicas.com.br',
                 port: '',
                 pathname: '/imagens/**',
             },
